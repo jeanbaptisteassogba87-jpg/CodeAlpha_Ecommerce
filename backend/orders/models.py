b28f01adf796d617f6545617d12b496ee14affe5
jeanbaptisteassogba87-jpg <jeanbaptisteassogba87@gmail.com>
2026-07-01 10:43:02 +0100

    Ajout du modèle OrderItem

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from users.models import User
 
+from products.models import Product
+
+
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     total = models.DecimalField(max_digits=10, decimal_places=2)
@@ -14,3 +17,12 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Commande #{self.id} - {self.user.name}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete= models.CASCADE)
+    product = models.ForeignKey(Product , on_delete= models.CASCADE)
+    quantity = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity}x {self.product.name} - {self.price}€"
