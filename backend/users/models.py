de856c301f687b700e73bd551d8ad874108598a0
jeanbaptisteassogba87-jpg <jeanbaptisteassogba87@gmail.com>
2026-07-01 09:58:09 +0100

    Ajout du modèle User

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model) :
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True, max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add= True)
+
+    class Meta :
+        ordering = ['-created_at']
+        verbose_name_plural = "Utilisateurs"
+
+    def __str__(self):
+        return f"{self.name}"
+
