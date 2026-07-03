5d0b2515621323cbe094c709b46d8acadbfe66da
jeanbaptisteassogba87-jpg <jeanbaptisteassogba87@gmail.com>
2026-07-03 12:37:49 +0100

    Ajout du serializer pour Order et OrderItem

---
+++ backend/orders/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Order , OrderItem
+from ..products.serializers import ProductSerializer
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    product_id = serializers.IntegerField(write_only=True)
+    class Meta:
+        model = OrderItem
+        fields = ['id','product','product_id','quantity','price']
+
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    user_name = serializers.CharField(source='user.name',read_only=True)
+    items = OrderItemSerializer(many=True , read_only=True)
+    class Meta :
+        model = Order
+        fields = "__all__"
