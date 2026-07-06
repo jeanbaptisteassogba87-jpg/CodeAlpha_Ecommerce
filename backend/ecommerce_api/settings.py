2a94db73e279383258eb4f02a5f7655dea7d9899
jeanbaptisteassogba87-jpg <jeanbaptisteassogba87@gmail.com>
2026-07-06 23:56:55 +0100

    Ajout de l'authentification JWT et sécurisation des endpoints API

--- backend/ecommerce_api/settings.py
+++ backend/ecommerce_api/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'orders',
     'products',
     'users',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from .models import User
 from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer 
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
