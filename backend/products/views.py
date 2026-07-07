b50024712a8045c3a9412c14a21304156fe969da
jeanbaptisteassogba87-jpg <jeanbaptisteassogba87@gmail.com>
2026-07-08 00:34:23 +0100

    Rend les produits accessibles publiquement

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,9 +1,9 @@
 from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
